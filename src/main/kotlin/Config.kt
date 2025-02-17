cbe306ca1aa565321835d9d9b1feb5bb4edc3885
Edwin Chang <edwinchang2024@gmail.com>
2025-02-16 20:14:59 -0800

    set config defaults

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -3,9 +3,9 @@ package dev.edwinchang
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Config(val syscalls: List<Syscall>, val ollama: Ollama) {
+data class Config(val syscalls: List<Syscall> = emptyList(), val ollama: Ollama = Ollama()) {
     @Serializable
-    data class Syscall(val code: Int, val run: String, val args: List<String>)
+    data class Syscall(val code: Int, val run: String, val args: List<String> = emptyList())
 
     @Serializable
     data class Ollama(val endpoint: String = "http://localhost:11434/api/chat", val model: String? = null)
